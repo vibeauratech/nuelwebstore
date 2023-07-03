6a93dda96d6fa3986eaecbc10b3ee9871788e2e1
emmanuelakharia <107748497+vibeauratech@users.noreply.github.com>
2023-07-03 23:55:34 +0100

    yes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
+import Home from './Pages/Home/Home';
+import Products from './Pages/Products/Products';
+import Product from './Pages/Product/Product';
 import './App.css';
+
+const Layout = () =>{
+  return(
+    <div className="main">
+      <Navbar />
+      <div className='topbar'></div>
+      <Outlet/>
+      <Footer />
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>,
+      },
+      {
+        path:"/products/:id",
+        element:<Products/>,
+      },
+      {
+        path:"/product/:id",
+        element:<Product/>,
+      },
+    ]
+  },
+
+])
 
 function App() {
   return (
     <div className="App">
-     
+      <RouterProvider router={router} />    
     </div>
   );
 }
 
 export default App;
+
