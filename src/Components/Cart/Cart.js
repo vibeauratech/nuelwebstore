478837844450cae593416a1fe33d576ce6e24775
emmanuelakharia <107748497+vibeauratech@users.noreply.github.com>
2023-07-05 11:42:40 +0100

    cart Update

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import "./Cart.css"
+
+function Cart() {
+   const data = [
+    {
+      id: 1,
+      img:"https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F30%2F6e%2F306e66f18d83c9532255579dc92ce19840ab2025.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D",
+      title: "Jacket",
+      isNew: true,
+      oldprice: 19,
+      price: 20
+    },
+    
+    {
+      id: 2,
+      img:"https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F4d%2Fc2%2F4dc2af2c15d84ec83e6112db9704df317353ff68.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url[file:/product/main]",
+      title: "Shoe",
+      isNew: true,
+      oldprice: 19,
+      price: 20
+    },
+   ];
+  
+  return (
+    <div className='cart'>
+     <div className='cart-header'>
+     <h1>My Items</h1>
+     <button>Reset Cart</button>
+     </div>
+       {data?.map(item=>(
+          <div className="item" key={item.id}>
+           <div className='item-text'>
+              <img src={item.img} className="" alt="" />
+              <div className='cart-details'>
+              <p>{item.title}</p>
+              <span className='price'>1 * ${item.price}</span>
+              </div>
+           </div>
+           <div className='delete'>Delete</div> 
+         </div>
+       ))}
+
+       <div className='total'>
+         <div className='sub-total'>
+            <span>Order Value</span>
+            <span>$545</span>
+         </div>
+
+         <div className='main-total'>
+           <span>Total</span>
+           <span>$550</span>
+         </div>
+        </div>
+
+        <button className='proceed'>Proceed to checkout</button>
+    </div>
+  );
+};
+
+export default Cart
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,18 +2,22 @@ import React from 'react'
 import { useState } from 'react'
 import {Link } from 'react-router-dom'
 import './Navbar.css'
+import Cart from '../../Components/Cart/Cart';
 import Menu from "./NavImages/menu.png";
 import CartIcon from "./NavImages/CartIcon.png";
 import SearchIcon from "./NavImages/SearchIcon.png";
 import User from "./NavImages/User.png";
-
 
 const Navbar = () => {
     const [showNavbar, setShowNavbar] = useState(false)
 
     const handleShowNavbar = () => {
       setShowNavbar(!showNavbar)
-    }
+    };
+
+    const [open,setOpen] = useState(false)
+
+
   
 
     return (
@@ -38,19 +42,23 @@ const Navbar = () => {
 
                 <div className='auth-flex'>
                   <div><img src={SearchIcon} className="nav-icon" alt="list Images" /></div>
-                  <div className='cartbox'>
+
+                  <div className='auth'><img src={User} className="nav-icon" alt="list Images" /></div> 
+                  
+                  <div className='cartbox' onClick={()=>setOpen(!open)}>
                     <img src={CartIcon} className="nav-icon" alt="list Images" />
                     <span>0</span>
                   </div>
-                  <div className='auth'><img src={User} className="nav-icon" alt="list Images" /></div> 
                 </div>                
-            </div>
-                
+            </div>             
 
             <div className="menu-icon" onClick={handleShowNavbar}>
              <img src={Menu} className="" alt="list Images" />
             </div>
         </div>
+
+        {open && <Cart/>}
+      
       </nav>
     )
   }
