63d2204f0be406c684f52de5e475e0f8b451ef9a
emmanuelakharia <107748497+vibeauratech@users.noreply.github.com>
2023-07-07 10:13:17 +0100

    Update Card.js

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import "./Card.css"
 import {Link } from 'react-router-dom'
-import Roadman from "./CardImages/RoadmanImage.png";
 import PreviewArrow from "./CardImages/PreviewArrow.png";
 
 function Card({item}) {
